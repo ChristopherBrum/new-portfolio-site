f27317152fbd61db7a6354cf06dc24a65057e50c
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-05-19 15:57:04 -0700

    menu hide and main body adjust together

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 const closeNav = () => {
-  const navToggle = document.querySelectorAll('.nav')[0]
+  const navToggle = document.querySelectorAll('.nav')[0];
+  const mainToggle = document.querySelectorAll('.main')[0];
   const menuToggle = document.querySelectorAll('.nav__list')[0];
   const headshotToggle = document.querySelectorAll('.nav__headshot')[0]; 
   const socialToggle = document.querySelectorAll('.nav__social-list')[0];
 
   navToggle.classList.toggle('nav--toggle');
+  mainToggle.classList.toggle('main--toggle');
   menuToggle.classList.toggle('nav__list--toggle');
   headshotToggle.classList.toggle('nav__headshot--toggle');
   socialToggle.classList.toggle('nav__social-list--toggle');
