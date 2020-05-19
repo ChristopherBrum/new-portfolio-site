0e1bda87006ea1aa5efe0ae0475a5fb974e92ae3
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-05-19 15:43:52 -0700

    menu toggle open/close

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 const closeNav = () => {
-  let closeBtn = document.getElementsByClassName('.nav')[0].style.width = "50px";
+  const navToggle = document.querySelectorAll('.nav')[0]
+  const menuToggle = document.querySelectorAll('.nav__list')[0];
+  const headshotToggle = document.querySelectorAll('.nav__headshot')[0]; 
+  const socialToggle = document.querySelectorAll('.nav__social-list')[0];
+
+  navToggle.classList.toggle('nav--toggle');
+  menuToggle.classList.toggle('nav__list--toggle');
+  headshotToggle.classList.toggle('nav__headshot--toggle');
+  socialToggle.classList.toggle('nav__social-list--toggle');
 }
+
