dde950889d9645642d40157b14151224d316d23e
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-05-22 09:55:48 -0700

    Nav open/close button tweaks

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 const closeNav = () => {
   const navToggle = document.querySelectorAll('.nav')[0];
+  const navBtnToggle = document.querySelectorAll('.nav__btn')[0];
   const mainToggle = document.querySelectorAll('.main')[0];
   const menuToggle = document.querySelectorAll('.nav__list')[0];
   const headshotToggle = document.querySelectorAll('.nav__headshot')[0]; 
   const socialToggle = document.querySelectorAll('.nav__social-list')[0];
 
   navToggle.classList.toggle('nav--toggle');
+  navBtnToggle.classList.toggle('nav__btn--toggle');
   mainToggle.classList.toggle('main--toggle');
   menuToggle.classList.toggle('nav__list--toggle');
   headshotToggle.classList.toggle('nav__headshot--toggle');
